e68faad28a0b09865502100f73ab21d93185b0f0
devin-lynch <devin.l.lynch@gmail.com>
2022-10-19 14:30:16 -0500

    Test printing head, tail, and head.next

--- list.py
+++ list.py
@@ -9,15 +9,67 @@ class Node:
     def __str__(self):
         return str(self.data)
 
-node_one = Node(1, None)
-# print(node_one)
-# manually inserting nodes
-node_two = Node(2, None)
-node_one.next = node_two
-print(f'first node: {node_one} seccond node: {node_one.next}')
+# node_one = Node(1, None)
+# # print(node_one)
+# # manually inserting nodes
+# node_two = Node(2, None)
+# node_one.next = node_two
+# print(f'first node: {node_one} seccond node: {node_one.next}')
 
-class Linkedlist:
+class LinkedList:
     '''
         singly linked list manager class
     '''
-   
+    def __init__(self):
+        # lists start out empty
+        self.head = None
+        self.tail = None
+        # how many nodes exist in the list
+        self.size = 0
+
+    def __len__(self):
+        # len(list) -> size of the list
+        return self.size
+
+    def __str__(self):
+        # return a meaningful string representation of our list
+        # iterate over the list, concatenate every node's value into a string
+        pass
+
+    def insert_front(self, data):
+        '''
+            create a new node and place it at the front of the list
+        '''
+        # if this is the first thing in the list
+        if len(self) == 0:
+            # set the new node to be both the head and the tail of the list
+            self.head = Node(data, None)
+            self.tail = self.head
+        # replace the head with the new node
+        else:
+            new_node = Node(data, self.head)
+            self.head = new_node
+        # increment the size of our list
+        self.size += 1
+
+    def insert_end(self):
+        '''
+            create a new node and place it at the end of the list
+        '''
+        pass
+
+    def insert_after(self):
+        '''
+            search for a node, and insert a new node after it
+        '''
+        pass
+
+
+
+my_list = LinkedList()
+print(len(my_list))
+my_list.insert_front(5)
+my_list.insert_front(4)
+my_list.insert_front(3)
+print(f'head: {my_list.head}, tail: {my_list.tail}, head.next: {my_list.head.next}')
+
